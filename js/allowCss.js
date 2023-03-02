3592df7b732bc8755150ed56a0e38b6fb36f744e
LambertPlsk <lambert.pliszka@gmail.com>
2023-03-02 19:13:22 +0100

    x

---
+++ js/allowCss.js
@@ -0,0 +1,5 @@
+(function(tags) {
+	for (var i = 0; i < tags.length; i++) {
+		document.createElement(tags[i]);
+	}
+})(['article', 'aside', 'audio', 'bdi', 'canvas', 'data', 'datalist', 'details', 'dialog', 'embed', 'figcaption', 'figure', 'footer', 'header', 'hgroup', 'keygen', 'main', 'mark', 'meter', 'nav', 'output', 'picture', 'progress', 'rp', 'rt', 'ruby', 'section', 'source', 'summary', 'template', 'time', 'track', 'wbr', 'video']);
---
+++ js/test.js
@@ -0,0 +1,6 @@
+
+elo = document.write("dziem dobryk");
+function two (jeden, dwa){
+    for (let jeden = 0; jeden <5; jeden++)
+    return ('wynik to ' + jeden);
+}
